eed24b90881582f3758a2f3b57c58fee177f110c
Borisivc <101225512+Borisivc@users.noreply.github.com>
2023-10-18 19:14:26 -0300

    get comunas

---
+++ get_comunas.php
@@ -0,0 +1,27 @@
+<?php
+include "include.php";
+
+if (isset($_GET['region'])) {
+    $regionId = $_GET['region'];
+
+    // Realiza una consulta SQL para obtener las comunas de la región
+    $query = "SELECT * FROM comuna WHERE id_region = ?";
+    $stmt = $conexion->prepare($query);
+    $stmt->bind_param("i", $regionId);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    $comunas = array();
+
+    while ($row = $result->fetch_assoc()) {
+        $comuna = array(
+            'id_comuna' => $row['id_comuna'],
+            'nombre_comuna' => $row['nombre_comuna']
+        );
+        $comunas[] = $comuna;
+    }
+
+    header('Content-Type: application/json');
+    echo json_encode($comunas);
+}
+?>
